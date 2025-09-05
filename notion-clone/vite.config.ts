0093527019596b25990a2273b513df5a99117877
kotake354 <tsunpo.777@gmail.com>
2025-09-05 16:12:43 +0900

    notion-clone環境構築と移行

--- notion-clone/vite.config.ts
+++ notion-clone/vite.config.ts
@@ -1,14 +1,20 @@
 import path from 'path';
-import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
 
 
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [tailwindcss()],
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
     },
   },
   resolve: {
